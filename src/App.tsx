73340ae917f9b07a7c2bbe1ce07ad94603d297f9
Jeje <jjh4450jjh4450@gmail.com>
2024-12-18 21:27:19 +0900

    feat: add routeing to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,38 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import React, { JSX, Suspense } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-function App() {
-  const [count, setCount] = useState(0);
+import Layout from "@/components/Layout";
+import { Loading } from "@/components/Loading";
+import { routerInfo } from "@/shared/routing/routerInfo.tsx";
 
+const lazyLoad = (
+  componets: () => Promise<{ default: React.ComponentType<unknown> }>,
+) => (
+  <Suspense fallback={<Loading />}>
+    {React.createElement(React.lazy(componets))}
+  </Suspense>
+);
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: routerInfo.map(({ path, element }) => ({
+      path,
+      element: lazyLoad(element),
+    })),
+  },
+]);
+
+/**
+ * The main application component.
+ * @returns {JSX.Element} The rendered application component.
+ */
+function App(): JSX.Element {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <RouterProvider router={router} />
+    </div>
   );
 }
 
