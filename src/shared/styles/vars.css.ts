41c569d188b3f7777e976d9aa528f17435cf4694
jjh4450 <jjh4450git@gmail.com>
2024-12-31 03:36:31 +0900

    feat add header

---
+++ src/shared/styles/vars.css.ts
@@ -0,0 +1,16 @@
+import { createGlobalTheme } from "@vanilla-extract/css";
+
+export const vars = createGlobalTheme("#root", {
+  color: {
+    blue1: "#0781CD",
+    blue2: "#029FD7",
+    blue3: "#C9CEE1",
+    green1: "#04D1C3",
+    green2: "#6AE5AE",
+    green3: "#B1F492",
+    white: "#FFFFFF",
+  },
+  font: {
+    pretendardRegular: "pretendard, sans-serif",
+  },
+});
